7648825759cd26a488b5fac637fdf237d08d12eb
jynam <118524209+JongYoung-Namgung@users.noreply.github.com>
2025-04-30 20:54:53 +0900

    Update __init__.py

--- apac/__init__.py
+++ apac/__init__.py
@@ -1,11 +1,2 @@
-
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
-#
-#   See COPYING file distributed along with the APAC package for the
-#   copyright and license terms.
-#
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
-
-
 from . import util
 from . import core
